258171b36e233353beb7bb2ab9882067722c23ab
Onyx Ganda <onyxganda@gmail.com>
2022-08-11 16:45:39 +0200

    PropDetail refactor into fn comp

--- src/components/PropDetail/index.jsx
+++ src/components/PropDetail/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 
 const renderList = ( list ) => {
     let result = <ul className="propDetail-value-container">
@@ -11,42 +11,82 @@ const renderList = ( list ) => {
     return result;
 }
 
-class PropDetail extends Component {
-    
-    render() {
-        const { propName, propType, value } = this.props;
-        var prop, detailClasses = "propDetail";
-        switch(propType) {
-            case "string":
-            case "number":
-                detailClasses = detailClasses.concat(" inline");
-                prop = <div className={detailClasses}>
-                    <span className="title">{propName}</span>
-                    <div className="propDetail-value">{value}</div>
-                </div>
-                break;
-            case "text":
-                prop = <div className={detailClasses}>
-                    <span className="title">{propName}</span>
-                    <div className="propDetail-value">{value}</div>
-                </div>
-                break;
-            case "list":
-                if ( !(value instanceof Array) ) throw new Error("PropDetail - For list prop types, value must be an array");
-                prop = <div className={detailClasses}>
-                    <span className="title">{propName}</span>
-                    {renderList(value)}
-                </div>
-                break;
-            default:
-                return null;
-        }
-        return(
-           <>
-            {prop}
-           </>
-        )
+const PropDetail = ({propName, propType, propNameMap, value}) => {
+    let prop, detailClasses = "propDetail";
+    switch(propType) {
+        case "string":
+        case "number":
+            detailClasses = detailClasses.concat(" inline");
+            prop = <div className={detailClasses}>
+                <span className="title">
+					{propNameMap[propName] || propName}
+				</span>
+                <div className="propDetail-value">{value}</div>
+            </div>
+            break;
+        case "text":
+            prop = <div className={detailClasses}>
+                <span className="title">
+					{propNameMap[propName] || propName}
+				</span>
+                <div className="propDetail-value">{value}</div>
+            </div>
+            break;
+        case "list":
+            if ( !(value instanceof Array) ) throw new Error("PropDetail - For list prop types, value must be an array");
+            prop = <div className={detailClasses}>
+                <span className="title">
+					{propNameMap[propName] || propName}
+				</span>
+                {renderList(value)}
+            </div>
+            break;
+        default:
+            return null;
     }
+    return(
+        <>
+         {prop}
+        </>
+    )
 }
+
+// class PropDetail extends Component {
+    
+//     render() {
+//         const { propName, propType, value } = this.props;
+//         var prop, detailClasses = "propDetail";
+//         switch(propType) {
+//             case "string":
+//             case "number":
+//                 detailClasses = detailClasses.concat(" inline");
+//                 prop = <div className={detailClasses}>
+//                     <span className="title">{propName}</span>
+//                     <div className="propDetail-value">{value}</div>
+//                 </div>
+//                 break;
+//             case "text":
+//                 prop = <div className={detailClasses}>
+//                     <span className="title">{propName}</span>
+//                     <div className="propDetail-value">{value}</div>
+//                 </div>
+//                 break;
+//             case "list":
+//                 if ( !(value instanceof Array) ) throw new Error("PropDetail - For list prop types, value must be an array");
+//                 prop = <div className={detailClasses}>
+//                     <span className="title">{propName}</span>
+//                     {renderList(value)}
+//                 </div>
+//                 break;
+//             default:
+//                 return null;
+//         }
+//         return(
+//            <>
+//             {prop}
+//            </>
+//         )
+//     }
+// }
 
 export default PropDetail;
