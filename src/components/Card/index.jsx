3f25e5f76c32cd325a9cfbfb1412923caf7a8c2b
Onyx Ganda <onyxganda@gmail.com>
2022-08-12 17:02:32 +0200

    Refactor to fn comp of Card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,41 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
 
 import Loader from "../Loader";
 
-class Card extends Component {
+const FnCard = ( props ) => {
+    const { id, list,
+        loading, title, size, openDetails, color,
+        next, previous
+    } = props;
 
-    render() {
-        const { id, list,
-            loading, title, size, openDetails, color,
-            next, previous
-        } = this.props;
+    // The following actually modifies the referenced object in the list passed
+    // by the parent component
+    // TODO: Check if is a doable practice in React
+    var data = list?.[id];
+    if (data) {
+        data.name = id;
+        data.next = next,
+        data.previous = previous;
+    } else data = { name: id };
+    
+    var cardClasses = size ? "card".concat(" "+size) : "card";
 
-        // The following actually modifies the referenced object in the list passed
-        // by the parent component
-        // TODO: Check if is a doable practice in React
-        var data = list?.[id];
-        if (data) {
-            data.name = id;
-            data.next = next,
-            data.previous = previous;
-        } else data = { name: id };
-        
-        var cardClasses = size ? "card".concat(" "+size) : "card";
-        return(
-            <div 
-                className={cardClasses}
-                onClick={() => openDetails(data)}
-            >
-                <Loader show={loading} />
-                <div className="card-hero" style={{
-                    backgroundColor: !data?.sprites?.other["official-artwork"]?.front_default ? color || "#999999" : null,
-                    backgroundImage: data?.sprites?.other["official-artwork"]?.front_default ? 
-                    "url("+data?.sprites.other["official-artwork"].front_default+")" : null
-                }}></div>
-                <span className="card-title">{title}</span>
-            </div>
-        )
-    }
+    return(
+        <div 
+            className={cardClasses}
+            onClick={() => openDetails(data)}
+        >
+            <Loader show={loading} />
+            <div className="card-hero" style={{
+                backgroundColor: !data?.sprites?.other["official-artwork"]?.front_default ? color || "#999999" : null,
+                backgroundImage: data?.sprites?.other["official-artwork"]?.front_default ? 
+                "url("+data?.sprites.other["official-artwork"].front_default+")" : null
+            }}></div>
+            <span className="card-title">{title}</span>
+        </div>
+    )
 }
 
-export default Card;
+export default FnCard;
