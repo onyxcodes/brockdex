829cddb49857c5ab635391f0ae61bc4b9e9a7fca
Onyx Ganda <onyxganda@gmail.com>
2022-08-12 23:20:40 +0200

    Refactor App to fn comp

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import ActionBar from "../components/ActionBar";
 import SearchBar from "../components/SearchBar";
@@ -7,16 +7,12 @@ import ListView from "../views/ListView";
 import FavoritesMgt from "../features/favoritesMgt";
 import PokeModal from "../views/PokeModal";
 
-// interface FavoritesActions {
-//     favoriteKey: string;
-
-// }
 const favoriteActionBarItems = ( favorites, favoriteKey, add, remove ) => {
     return [
         { 
             item: !favorites.includes(favoriteKey) ? 
-            <button onClick={() => add(favoriteKey)}>⭐ Add</button> :
-            <button onClick={() => remove(favoriteKey)}>⭐ Remove</button>, position: "right"
+            <button onClick={() => add()}>⭐ Add</button> :
+            <button onClick={() => remove()}>⭐ Remove</button>, position: "right"
         }
     ];
 }
@@ -34,22 +30,19 @@ const listMoveActionBarItems = ( pokemon, changeContent ) => {
     ]
 }
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showModal: false,
-            focusedElement: null,
-            searchQuery: null,
-            total: null,
-            favoritesMgt: new FavoritesMgt(),
-            favorites: null,
-            favoritesVisible: false,
-            list: {},
-        }
-    }
+const App = () => {
+    const [ modalVisible, showModal ] = React.useState(false);
+    const [ focusedElement, setFocusedEl ] = React.useState(null);
+    const [ searchQuery, setSearchQuery ] = React.useState(null);
+    const [ total, setTotal ] = React.useState(null); // TODO: consider setting 0 as init val
+    const [ favoritesMgt , setFavoritesMgt ] = React.useState(new FavoritesMgt());
+    // TODO: consider using another state property to determine whether to know that is showing favorites list
+    // and therefore disable search features
+    const [ favoritesShown, setFavoritesVisible ] = React.useState(false);
+    const [ list, setList ] = React.useState({});
 
-    updateTotal( total ) {
+    // Updates total count stored in localStorage with given total, if differs
+    const updateTotal = ( total ) => {
         let storedTotal = localStorage.getItem("total") ? Number(localStorage.getItem("total")) : null;
         if ( storedTotal && // total already stored
             storedTotal !== total && // update only when different
@@ -63,84 +56,61 @@ class App extends Component {
         } // else // skip update cause not needed
     }
 
-    setPokemonList( list, total ) {
-        this.updateTotal(total);
-        this.setState({
-            list: list
-        })
+    // Updates with total number of pokemon, only once when list first loads
+    React.useEffect( () => updateTotal(total), [total] );
+
+    // The PokeModal will be shown/hidden based on focusedElement presence
+    // and current modal state (if is already shown won't trigger)
+    React.useEffect( () => {
+        ( !modalVisible || !focusedElement ) && showModal(!!focusedElement);
+    }, [focusedElement]);
+
+    // When called, adds currently focused el to favorites
+    const addToFavorites = () => {
+        let favorites = favoritesMgt.addToFavorites(focusedElement.name);
+        setFavoritesMgt(new FavoritesMgt(favorites));
     }
 
-    setSearchQuery(name) {
-        this.setState({ searchQuery: name })
+    // When called, removes currently focused el to favorites
+    const removeFromFavorites = () => {
+        let favorites = favoritesMgt.removeFromFavorites(focusedElement.name);
+        setFavoritesMgt(new FavoritesMgt(favorites));
     }
 
-    openDetails( element ) {
-        this.setState({
-            focusedElement: element
-        }, () => {
-            this.setState({
-                showModal: true
-            })
-        })
-    }
-
-    changeContent( elementName ) {
-        let element = this.state.list[elementName];
-        this.setState({
-            showModal: false
-        }, () => this.openDetails(element));
-    }
-
-    closeModal() {
-        this.setState({
-            showModal: false
-        })
-    }
-
-    addToFavorites(elementId) {
-        let favorites = this.state.favoritesMgt.addToFavorites(elementId);
-        this.setState({ favoritesMgt: new FavoritesMgt(favorites) })
-    }
-
-    removeFromFavorites(elementId) {
-        let favorites = this.state.favoritesMgt.removeFromFavorites(elementId);
-        this.setState({ favoritesMgt: new FavoritesMgt(favorites) })
-    }
-
-    render() {
-        return (
-            <div id="app">
-                <ActionBar bgColor="blueviolet" position="top" items={[
-                    { item: <SearchBar value={this.state.searchQuery} disabled={this.state.favoritesVisible} setSearchQuery={(name)=> this.setSearchQuery(name)}/>, position: "left" },
-                    { item: <span>BrockDex</span>, position: "center" },
-                    { item: <button>Favs</button>, position: "right" }
-                ]} />
-                <ListView 
-                    size="medium"
-                    query={this.state.searchQuery}
-                    total={localStorage.getItem("total")}
-                    setPokemonList={(list) => this.setPokemonList(list)}
-                    openDetails={(el) => this.openDetails(el)}
-                />
-                { this.state.showModal ? <PokeModal
-                    id={this.state.focusedElement?.id}
-                    name={this.state.focusedElement?.name}
-                    visible={this.state.showModal}
-                    closeModal={() => this.closeModal()}
-                    topActionBarItems={favoriteActionBarItems(
-                        this.state.favoritesMgt.getFavorites(),
-                        this.state.focusedElement?.name,
-                        (elId) => this.addToFavorites(elId),
-                        (elId) => this.removeFromFavorites(elId)
-                    )}
-                    getBtmBarItems={(element) => listMoveActionBarItems(element, (element) => this.changeContent(element))}
-                    changeContent={(element) => this.changeContent(element)}
-                    data={this.state.focusedElement}
-                    size="medium"
-                /> : null }
-            </div>
-        )
-    }
+    return (<div id="app">
+        <ActionBar bgColor="blueviolet" position="top" items={[
+            { 
+                item: <SearchBar 
+                    value={searchQuery} 
+                    disabled={favoritesShown} 
+                    setSearchQuery={(name)=> setSearchQuery(name)}
+                />,
+                position: "left" 
+            },
+            { item: <span>BrockDex</span>, position: "center" },
+            { item: <button>Favs</button>, position: "right" }
+        ]} />
+        <ListView 
+            query={searchQuery}
+            total={localStorage.getItem("total")}
+            setPokemonList={(list, total) => { setList(list); setTotal(total) }}
+            openDetails={(el) => setFocusedEl(el)}
+        />
+        { modalVisible && <PokeModal
+            id={focusedElement?.id}
+            name={focusedElement?.name}
+            visible={modalVisible}
+            closeModal={() => setFocusedEl(null)}
+            topActionBarItems={favoriteActionBarItems(
+                favoritesMgt.getFavorites(),
+                focusedElement?.name,
+                () => addToFavorites(),
+                () => removeFromFavorites()
+            )}
+            getBtmBarItems={(element) => listMoveActionBarItems(element, (element) => setFocusedEl(list[element]))}
+            data={focusedElement}
+        /> }
+    </div>)
 }
 
 export default App;
