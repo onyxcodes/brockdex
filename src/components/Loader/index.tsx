0f8171efa4e6de2f41d3fc0893bbd74cb9b84526
Onyx Ganda <onyxganda@gmail.com>
2022-08-12 18:29:14 +0200

    Refactor Loader to TS

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/* TODO: make it a more stylish loader 
+ * Consider localization meanwhile
+*/
+
+const Loader = ( props: { show: boolean } ) => {
+    const { show = false } = props;
+    return (
+        show && <div className="loader">Loading</div>
+    );
+}
+
+export default Loader;
